55876aa142c73cdb855f8c177113d4e48d9df6e0
quanghien <nquanghien97@gmail.com>
2025-04-18 10:22:28 +0700

    fix

--- zustand/file-categories.ts
+++ zustand/file-categories.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
-import Cookies from "js-cookie";
-import { verifyToken } from "@/lib/token";
+// import Cookies from "js-cookie";
+// import { verifyToken } from "@/lib/token";
 import { getFileCategories } from "@/services/file-categories";
 import { FileCategoriesEntity } from "@/entities/file-categories";
 
@@ -13,12 +13,12 @@ export const useFileCategories = create<FileCategoriesType>()((set) => ({
   fileCategories: null,
   getFileCategories: async () => {
     try {
-      const token = Cookies.get('token');
-      const dataParse = await verifyToken(token || '')
-      if(dataParse?.user_id) {
+      // const token = Cookies.get('token');
+      // const dataParse = await verifyToken(token || '')
+      // if(dataParse?.user_id) {
         const res = await getFileCategories()
         set({ fileCategories: res.fileCategories })
-      }
+      // }
     } catch (err) {
       console.log(err)
     }
